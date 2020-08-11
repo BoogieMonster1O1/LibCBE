19f7c372039cce3a60ce293604ef6aaf75c22a47
SD <harrypotter.starwars.shrish@gmail.com>
2020-08-11 19:19:47 +0530

    Added files
     Changes to be committed:
    	modified:   .gitignore
    	modified:   build.gradle
    	new file:   src/main/java/io/github/boogiemonster1o1/libcbe/impl/LibCBEInitializer.java
    	deleted:    src/main/java/net/fabricmc/example/ExampleMod.java
    	deleted:    src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
    	modified:   src/main/resources/fabric.mod.json
    	renamed:    src/main/resources/modid.mixins.json -> src/main/resources/libcbe.mixins.json

---
+++ src/main/java/io/github/boogiemonster1o1/libcbe/impl/LibCBEInitializer.java
@@ -0,0 +1,10 @@
+package io.github.boogiemonster1o1.libcbe.impl;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LibCBEInitializer implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
