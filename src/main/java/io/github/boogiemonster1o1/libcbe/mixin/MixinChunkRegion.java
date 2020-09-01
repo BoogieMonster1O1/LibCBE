4cffda2448ddc2f70a32c39091ede3abd8eb3275
SD <harrypotter.starwars.shrish@gmail.com>
2020-09-01 19:31:18 +0530

    Removed dummy injects
     Changes to be committed:
    	modified:   src/main/java/io/github/boogiemonster1o1/libcbe/mixin/MixinChunkRegion.java
    	modified:   src/main/java/io/github/boogiemonster1o1/libcbe/mixin/MixinWorldChunk.java

--- src/main/java/io/github/boogiemonster1o1/libcbe/mixin/MixinChunkRegion.java
+++ src/main/java/io/github/boogiemonster1o1/libcbe/mixin/MixinChunkRegion.java
@@ -3,18 +3,12 @@ package io.github.boogiemonster1o1.libcbe.mixin;
 import io.github.boogiemonster1o1.libcbe.api.ConditionalBlockEntityProvider;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.ChunkRegion;
@@ -22,24 +16,12 @@ import net.minecraft.world.chunk.Chunk;
 
 @Mixin(ChunkRegion.class)
 public abstract class MixinChunkRegion implements BlockView {
-    @Shadow public abstract BlockState getBlockState(BlockPos pos);
-
-    @Inject(method = "getBlockEntity", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/block/BlockEntityProvider;createBlockEntity(Lnet/minecraft/world/BlockView;)Lnet/minecraft/block/entity/BlockEntity;"), locals = LocalCapture.CAPTURE_FAILEXCEPTION)
-    public void getBlockEntity(BlockPos pos, CallbackInfoReturnable<BlockEntity> cir, Chunk chunk, BlockEntity blockEntity, CompoundTag compoundTag, BlockState blockState, Block block) {
-        if(block instanceof ConditionalBlockEntityProvider) {
-            ConditionalBlockEntityProvider cbeBlock = (ConditionalBlockEntityProvider) block;
-            if(!cbeBlock.hasBlockEntity(blockState) && !cbeBlock.hasBlockEntity(pos, this)) {
-                blockEntity = null;
-            }
-        }
-    }
-
     @Redirect(method = "setBlockState", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/chunk/Chunk;setBlockEntity(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/entity/BlockEntity;)V"))
     public void setBlockState(Chunk chunk, BlockPos pos, BlockEntity blockEntity, BlockPos blockPos, BlockState state, int flags, int maxUpdateDepth) {
         if(this.getBlockState(pos).getBlock() instanceof ConditionalBlockEntityProvider) {
             ConditionalBlockEntityProvider cbeBlock = (ConditionalBlockEntityProvider) this.getBlockState(pos).getBlock();
-            if(!cbeBlock.hasBlockEntity(state) && !cbeBlock.hasBlockEntity(pos, this)) {
-                blockEntity = null;
+            if(cbeBlock.hasBlockEntity(state) || cbeBlock.hasBlockEntity(pos, this)) {
+                chunk.setBlockEntity(pos, ((BlockEntityProvider) this.getBlockState(pos).getBlock()).createBlockEntity(this));
             }
         } else {
             chunk.setBlockEntity(pos, ((BlockEntityProvider) this.getBlockState(pos).getBlock()).createBlockEntity(this));
--- src/main/java/io/github/boogiemonster1o1/libcbe/mixin/MixinWorldChunk.java
+++ src/main/java/io/github/boogiemonster1o1/libcbe/mixin/MixinWorldChunk.java
@@ -7,62 +7,39 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.WorldChunk;
 
 @Mixin(WorldChunk.class)
-public abstract class MixinWorldChunk {
+public abstract class MixinWorldChunk implements Chunk {
     @Shadow public abstract BlockState getBlockState(BlockPos pos);
 
     @Shadow @Final private World world;
-
-    @Inject(method = "loadBlockEntity", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/block/BlockEntityProvider;createBlockEntity(Lnet/minecraft/world/BlockView;)Lnet/minecraft/block/entity/BlockEntity;"), locals = LocalCapture.CAPTURE_FAILEXCEPTION, cancellable = true)
-    public void loadBlockEntity(BlockPos pos, CompoundTag tag, CallbackInfoReturnable<BlockEntity> cir, BlockEntity blockEntity, BlockState blockState, Block block) {
-        if(block instanceof ConditionalBlockEntityProvider) {
-            if(!((ConditionalBlockEntityProvider) block).hasBlockEntity(blockState) && !((ConditionalBlockEntityProvider) block).hasBlockEntity(pos, (BlockView) this)) {
-                blockEntity = null;
-            }
-        }
-    }
 
     @Inject(method = "createBlockEntity", at = @At("RETURN"), cancellable = true)
     public void createBlockEntity(BlockPos pos, CallbackInfoReturnable<BlockEntity> cir) {
         if(this.getBlockState(pos).getBlock() instanceof ConditionalBlockEntityProvider) {
             ConditionalBlockEntityProvider cbeBlock = (ConditionalBlockEntityProvider) this.getBlockState(pos).getBlock();
-            if(!cbeBlock.hasBlockEntity(this.getBlockState(pos)) && !cbeBlock.hasBlockEntity(pos, (BlockView) this)) {
+            if(!cbeBlock.hasBlockEntity(this.getBlockState(pos)) && !cbeBlock.hasBlockEntity(pos, this)) {
                 cir.setReturnValue(null);
             }
         }
     }
 
-//    @Inject(method = "setBlockState", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/block/BlockEntityProvider;createBlockEntity(Lnet/minecraft/world/BlockView;)Lnet/minecraft/block/entity/BlockEntity;"), cancellable = true, locals = LocalCapture.CAPTURE_FAILEXCEPTION)
-//    public void setBlockState(BlockPos pos, BlockState state, boolean moved, CallbackInfoReturnable<BlockState> cir, int i, int j, int k, ChunkSection chunkSection, boolean bl, BlockState blockState, Block block, BlockEntity blockEntity2) {
-//        if(this.getBlockState(pos).getBlock() instanceof ConditionalBlockEntityProvider) {
-//            ConditionalBlockEntityProvider cbeBlock = (ConditionalBlockEntityProvider) this.getBlockState(pos).getBlock();
-//            try {
-//                if(!cbeBlock.hasBlockEntity(blockState) && !cbeBlock.hasBlockEntity(pos, (BlockView) this)) {
-//                    blockEntity2 = null;
-//                }
-//            } catch (IllegalArgumentException ignored) {
-//            }
-//        }
-//    }
-
     @Redirect(method = "setBlockState", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockEntity(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/entity/BlockEntity;)V"))
     public void setBlockState(World world, BlockPos pos, BlockEntity blockEntity, BlockPos pos2, BlockState state, boolean moved) {
         if(state.getBlock() instanceof ConditionalBlockEntityProvider) {
             ConditionalBlockEntityProvider cbeBlock = (ConditionalBlockEntityProvider) state.getBlock();
-            if(cbeBlock.hasBlockEntity(state) || cbeBlock.hasBlockEntity(pos, (BlockView) this)) {
+            if(cbeBlock.hasBlockEntity(state) || cbeBlock.hasBlockEntity(pos, this)) {
                 this.world.setBlockEntity(pos, blockEntity);
             }
         } else {
